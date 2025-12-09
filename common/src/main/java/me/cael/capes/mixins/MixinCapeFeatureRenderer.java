bdccce2ea4e9215a4f3499df50c98106c38762c3
CaelTheColher <16297430+CaelTheColher@users.noreply.github.com>
2025-12-09 14:56:15 -0300

    1.21.11 moment

--- common/src/main/java/me/cael/capes/mixins/MixinCapeFeatureRenderer.java
+++ common/src/main/java/me/cael/capes/mixins/MixinCapeFeatureRenderer.java
@@ -3,6 +3,7 @@ package me.cael.capes.mixins;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderLayers;
 import net.minecraft.client.render.entity.feature.CapeFeatureRenderer;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(CapeFeatureRenderer.class)
 public class MixinCapeFeatureRenderer {
 
-    @WrapOperation(method = "render*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderLayer;getEntitySolid(Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;"))
+    @WrapOperation(method = "render*", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/RenderLayers;entitySolid(Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;"))
     private RenderLayer fixCapeTransparency(Identifier texture, Operation<RenderLayer> original) {
-        return RenderLayer.getArmorCutoutNoCull(texture);
+        return RenderLayers.armorCutoutNoCull(texture);
     }
 }
